7d5b3693243634424a88d4d1d864e6d56d5f9f4f
ELESSARX <kdephp@gmail.com>
2022-08-28 19:38:08 +0300

    Plus

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,17 @@
 window.addEventListener('load', function(){
-   
+
 	let inp1 = document.querySelector('.inp1');
 	let inp2 = document.querySelector('.inp2');
 	let btn = document.querySelector('.go');
 	let res = document.querySelector('.res');
+	let operation = document.querySelector('select');
+
+	if (operation.value === '+') {
+		console.log(operation.value);
+	}
 
 	btn.addEventListener('click', function(){
+
 		let sum = parseInt(inp1.value) + parseInt(inp2.value);
 		res.innerHTML = sum;
 	});
@@ -20,9 +26,9 @@ window.addEventListener('load', function(){
 	btn.addEventListener('click', function(){
 		let divide = parseInt(inp1.value) / parseInt(inp2.value);
 		res.innerHTML = divide;
-		console.log(divide);
+
 	});
-	
+
 
 });
 
