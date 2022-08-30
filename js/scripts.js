83f8c4fd78e0ad8885ab56e25a809d8522cea0be
yevmuratova <yevgeniya.muratova@chumbani.shop>
2022-08-30 19:19:13 +0300

    1 task

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,40 @@
 window.addEventListener('load', function(){
 
-	let inp1 = document.querySelector('.inp1');
+	let inp1 = document.querySelector('.inp1'); 
 	let inp2 = document.querySelector('.inp2');
 	let btn = document.querySelector('.go');
-	let res = document.querySelector('.res');
+	let result = document.querySelector('.result');
 	let operation = document.querySelector('select');
 
-	if (operation.value === '+') {
-		console.log(operation.value);
+	
+	if (operation.value === 'add') {
+		btn.addEventListener('click', function(){
+			let sum = parseInt(inp1.value) + parseInt(inp2.value);
+			result.innerHTML = sum;
+		});
 	}
+	else if (operation.value === 'subtract') {
+		btn.addEventListener('click', function(){
+			let sub = parseInt(inp1.value) - parseInt(inp2.value);
+			result.innerHTML = sub;
+		});
+	}
+	else if (operation.value === 'multiply') {
+		btn.addEventListener('click', function(){
+			let mult = parseInt(inp1.value) * parseInt(inp2.value);
+			result.innerHTML = mult;
+		});
+	}
+	else {
+		btn.addEventListener('click', function(){
+			let div = parseInt(inp1.value) / parseInt(inp2.value);
+			result.innerHTML = div;
+		});
+	}
+	//result
+	console.log('${inp1.value} ${operator} ${inp2.value} = ${result}')
 
-	btn.addEventListener('click', function(){
-
-		let sum = parseInt(inp1.value) + parseInt(inp2.value);
-		res.innerHTML = sum;
-	});
-	btn.addEventListener('click', function(){
-		let min = parseInt(inp1.value) - parseInt(inp2.value);
-		res.innerHTML = min;
-	});
-	btn.addEventListener('click', function(){
-		let mult = parseInt(inp1.value) * parseInt(inp2.value);
-		res.innerHTML = mult;
-	});
-	btn.addEventListener('click', function(){
-		let divide = parseInt(inp1.value) / parseInt(inp2.value);
-		res.innerHTML = divide;
-
-	});
+	
 
 
 });
